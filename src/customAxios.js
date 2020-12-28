b9d2956e46ddcf9d5bf158bd7237eed9f6278766
ev15963 <ev15963@hs.ac.kr>
2020-12-29 01:31:32 +0900

    backend에게 static resource 전송 01:31

--- src/customAxios.js
+++ src/customAxios.js
@@ -9,8 +9,8 @@ export default function customAxios(url, callback) {
      * 개발 환경에서의 크료스 도메인 이슈를 해결하기 위한 코드로
      * 운영 환경에 배포할 경우 15~16
      */
-    // baseURL: "http://localhost:8282",
-    // withCredentials: true,
+    baseURL: "http://localhost:8282",
+    withCredentials: true,
   })
     .then(function (response) {
       callback(response.data);
@@ -20,13 +20,4 @@ export default function customAxios(url, callback) {
       console.error(e + " console err");
       console.log(e + " console err");
     });
-
-  // 간단한 HTTP GET Request
-  // const http_req = new XMLHttpRequest();
-
-  // http_req.open("GET", "http://localhost:8282/");
-  // http_req.send();
-
-  // http_req.onload = () =>
-  //   console.log("Flask 서버로 부터의 응답은: " + http_req.responseText);
 }
