82385cd43ef8f96dd3be266e581cfcde27c9914c
ev15963 <ev15963@hs.ac.kr>
2021-01-20 23:53:13 +0900

    axios 변경 23:53

--- src/customAxios.js
+++ src/customAxios.js
@@ -1,19 +1,19 @@
 import axios from "axios"; //엑시오스
 
 export default function customAxios(url, callback) {
-  axios
-    .get("http://localhost:8282/backend_202012-1/api" + url, {
-      headers: { "Access-Control-Allow-Origin": "*" },
-      //url: "/api" + url,
-      //method: "get",
+  axios({
+    //"http://localhost:8282/backend_202012-1/api" + url, {
+    headers: { "Access-Control-Allow-Origin": "*" },
+    url: "/api" + url,
+    method: "get",
 
-      /**
-       * 개발 환경에서의 크료스 도메인 이슈를 해결하기 위한 코드로
-       * 운영 환경에 배포할 경우 15~16
-       */
-      //baseURL: "http://localhost:8282/backend_202012-1",
-      withCredentials: true,
-    })
+    /**
+     * 개발 환경에서의 크료스 도메인 이슈를 해결하기 위한 코드로
+     * 운영 환경에 배포할 경우 15~16
+     */
+    baseURL: "http://localhost:8282/backend_202012-1",
+    withCredentials: true,
+  })
     .then(function (response) {
       callback(response.data());
       console.log(response + " response");
